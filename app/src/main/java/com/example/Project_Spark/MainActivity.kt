95e61fc54db1937ebfdac7a1d7d3b7c2770a3118
sanghoon3203 <starnote320389@gmail.com>
2024-05-17 03:42:43 +0900

    googleloginupdate

--- app/src/main/java/com/example/Project_Spark/MainActivity.kt
+++ app/src/main/java/com/example/Project_Spark/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.Project_Spark
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnGoToLogin = findViewById<Button>(R.id.btnGoToLogin)
+        btnGoToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
